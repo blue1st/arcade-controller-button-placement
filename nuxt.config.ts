e20d3ddc25bb50c816dac57c83f1c8f94505160f
blue1st <mail@example.com>
2025-08-05 00:00:50 +0900

    ページタイトル等追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 export default defineNuxtConfig({
+
   css: ["bulma/css/bulma.min.css"],
   app: {
+    head: {
+      title: 'Arcade Controller Button Placement',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
+    },
     cdnURL: '/arcade-controller-button-placement/'
   }
 })
