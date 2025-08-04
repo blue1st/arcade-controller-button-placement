a7b45489114878991be37f5c2dd3837fe72dcf48
blue1st <mail@example.com>
2025-08-04 20:13:20 +0900

    baseURL to cdnURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@ import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   css: ["bulma/css/bulma.min.css"],
   app: {
-    baseURL: '/arcade-controller-button-placement/'
+    cdnURL: '/arcade-controller-button-placement/'
   }
 })
